3d3f7126322849df4fcf07bcc9e626bcb42ea322
paulogermanopg <paulogermano.123pg@hotmail.com>
2020-03-12 15:54:24 -0300

    ok

--- src/screens/TaskList.js
+++ src/screens/TaskList.js
@@ -1,19 +1,34 @@
 import React, { Component } from 'react'
 import { View, Text, ImageBackground, StyleSheet } from 'react-native'
 
+import * as Font from 'expo-font'
 import todayImage from '../../assets/imgs/today.jpg'
 
 import moment from 'moment'
 import 'moment/locale/pt-br'
 
 export default class TaskList extends Component {
+    state = {
+        loading: true
+    }
+    async componentDidMount() {
+        await Font.loadAsync({
+          'Lato': require('../../assets/fonts/TravelingTypewriter.ttf'),
+        })
+        this.setState({ loading: false })
+    }
     render() {
+        if (this.state.loading) {
+            return (
+              <View></View>
+            );
+        }
         const today = moment().locale('pt-br').format('ddd, D [de] MMMM')
         return (
             <View style={styles.container}>
                 <ImageBackground source={todayImage} style={styles.background}>
                     <View style={styles.titleBar}>
-                        <Text>Hoje</Text>
+                        <Text style={{fontFamily: 'Lato', fontSize: 30}}>Hoje</Text>
                         <Text>{today}</Text>
                     </View>
                 </ImageBackground>
