7c2cf1645ceaff22a7b687fc23a1f79244aed0f7
paulogermanopg <paulogermano.123pg@hotmail.com>
2020-03-15 14:47:59 -0300

    modal02

--- src/commonStyles.js
+++ src/commonStyles.js
@@ -1,6 +1,7 @@
 export default {
     fontfamily: 'Lato',
     color: {
+        today: '#B13B44',
         secondary: '#FFF',
         mainText: '#222',
         subText: '#555',
--- src/screens/AddTask.js
+++ src/screens/AddTask.js
@@ -1,11 +1,21 @@
 import React, { Component } from 'react'
 import { Text, View, Modal, StyleSheet, TouchableWithoutFeedback } from 'react-native'
 
+import CommonStyles from '../commonStyles'
+
 export default class AddTaks extends Component {
     render() {
         return (
             <Modal transparent={true} visible={this.props.isVisible} onRequestClose={this.props.onCancel}
                 animationType='slide'>
+                <TouchableWithoutFeedback onPress={this.props.onCancel}>
+                    <View style={styles.background}>
+
+                    </View>
+                </TouchableWithoutFeedback>
+                <View style={styles.container}>
+                    <Text style={styles.header}>Nova Tarefa</Text>
+                </View>
                 <TouchableWithoutFeedback onPress={this.props.onCancel}>
                     <View style={styles.background}>
 
@@ -17,8 +27,20 @@ export default class AddTaks extends Component {
 }
 
 const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: '#FFF',
+    },
     background: {
         flex: 1,
         backgroundColor: 'rgba(0, 0, 0, 0.7)',
+    },
+    header: {
+        fontFamily: CommonStyles.fontfamily,
+        backgroundColor: CommonStyles.color.today,
+        color: CommonStyles.color.secondary,
+        textAlign: 'center',
+        padding: 15,
+        fontSize: 18,
     }
 })
