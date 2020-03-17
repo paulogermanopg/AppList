2ac601acc940b9459722e1c8df09b9721f521b2b
paulogermanopg <paulogermano.123pg@hotmail.com>
2020-03-17 14:44:47 -0300

    excluir

--- src/components/Task.js
+++ src/components/Task.js
@@ -1,8 +1,9 @@
 import React from 'react'
-import { View, Text, StyleSheet, TouchableWithoutFeedback, TouchableWithoutFeedbackBase } from 'react-native'
+import { View, Text, StyleSheet, TouchableWithoutFeedback, TouchableWithoutFeedbackBase, TouchableOpacity } from 'react-native'
 import CommonStyles from '../commonStyles'
 import Icon from 'react-native-vector-icons/FontAwesome'
 
+import Swipeable from 'react-native-gesture-handler/Swipeable'
 import moment from 'moment'
 import 'moment/locale/pt-br'
 
@@ -14,8 +15,26 @@ export default props => {
     const date = props.doneAt ? props.doneAt : props.estimateAt
     const formattedDate = moment(date).locale('pt-br').format('ddd, D [de] MMMM')
 
+    const getRightContent = () => {
+        return (
+            <TouchableOpacity style={styles.right}>
+                <Icon name="trash" size={30} color="#FFF" />
+            </TouchableOpacity>
+        )
+    }
+
+    const getLeftContent = () => {
+        return (
+            <View style={styles.left}>
+                <Icon name="trash" size={20} color="#FFF" style={styles.excludeIcon} />
+                <Text style={styles.excludeText}>Excluir</Text>
+            </View>
+        )
+    }
+
     return (
-        <View style={styles.container}>
+        <Swipeable renderRightActions={getRightContent} renderLeftActions={getLeftContent}>
+            <View style={styles.container}>
             <TouchableWithoutFeedback onPress={() => props.toggleTask(props.id)}>
                 <View style={styles.checkContainer}>
                     {getCheckView(props.doneAt)}
@@ -25,8 +44,9 @@ export default props => {
                 <Text style={[styles.desc, doneOrNot]}>{props.desc}</Text>
                 <Text style={styles.date}>{formattedDate + ""}</Text>
             </View>
-            
         </View>
+        </Swipeable>
+        
     )
 }
 
@@ -52,6 +72,7 @@ const styles = StyleSheet.create({
         borderBottomWidth: 1,
         alignItems: 'center',
         paddingVertical: 10,
+        backgroundColor: '#FFF',
     },
     checkContainer: {
         width: '20%',
@@ -82,5 +103,27 @@ const styles = StyleSheet.create({
         fontFamily: CommonStyles.fontfamily,
         color: CommonStyles.color.subText,
         fontSize: 12,
+    },
+    right: {
+        backgroundColor: 'red',
+        flexDirection: 'row',
+        alignItems: 'center',
+        justifyContent: 'flex-end',
+        paddingHorizontal: 20,
+    },
+    left: {
+        flex: 1,
+        backgroundColor: 'red',
+        flexDirection: 'row',
+        alignItems: 'center',
+    },
+    excludeText: {
+        fontFamily: CommonStyles.fontfamily,
+        color: '#FFF',
+        fontSize: 20,
+        margin: 10,
+    },
+    excludeIcon: {
+        marginLeft: 10,
     }
 })
